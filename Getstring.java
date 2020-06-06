b41e574b80aedf466bc63587e8d1ef762a5ec730
201802011 <66462318+201802011@users.noreply.github.com>
2020-06-06 12:47:55 +0530

    String Java  Program

---
+++ Getstring.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Getstring{
+public static void main(String[]args){
+Scanner dx=new Scanner(System.in);
+System.out.println("Enter a string:");
+String str=dx.nextLine();
+System.out.println("Enter b string:");
+String str1=dx.nextLine();
+System.out.println("A string:"+str);
+System.out.println("B string:"+str1);
+}
+}
