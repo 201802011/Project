389672421b38d60dff1283ed8d1fa45b3b1c5581
201802011 <66462318+201802011@users.noreply.github.com>
2020-06-06 13:37:16 +0530

    First Assignement

---
+++ Sum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Sum{
+public static void main(String[]args){
+int x,y,z;
+Scanner dx=new Scanner(System.in);
+System.out.println("Enter a number:");
+x=dx.nextInt();
+System.out.println("Enter b number:");
+y=dx.nextInt();
+z= x + y; 
+System.out.println("Sum of two value:"+z);
+}
+}
---
+++ wipro.java
@@ -0,0 +1,6 @@
+class wipro
+{
+public static void main(String[]args){
+System.out.println("wipro technologies bangalore");
+}
+}    
